cc4a9b54ba0881fe1c162bd28a62be833372cf3f
FRostri <FRostri@protonmail.com>
2021-03-26 21:39:54 -0500

    test: Add test and change tokens name for constant The tests doesn't works properly yet.

--- fol/lexer.py
+++ fol/lexer.py
@@ -8,10 +8,9 @@ from fol.token import (
 
 
 class Lexer:
-
     def __init__(self, source: str) -> None:
         self._source: str = source
-        self._character: str = ''
+        self._character: str = ""
         self._read_position: int = 0
         self._position: int = 0
         self._line: int = 1
@@ -21,70 +20,73 @@ class Lexer:
     def next_token(self) -> Token:
         self._skip_whitespace()
         # Token '='
-        if match(r'^=$', self._character):
-            if self._peek_character() == '=':
-                token = self._make_two_character_token(TokenType.Equals)
+        if match(r"^=$", self._character):
+            if self._peek_character() == "=":
+                token = self._make_two_character_token(TokenType.EQ)
             else:
-                token = Token(TokenType.Assign, self._character, self._line)
+                token = Token(TokenType.ASSIGN, self._character, self._line)
         # Token '+'
-        elif match(r'^\+$', self._character):
-            token = Token(TokenType.Plus, self._character, self._line)
+        elif match(r"^\+$", self._character):
+            token = Token(TokenType.PLUS, self._character, self._line)
         # Token '-'
-        elif match(r'^-$', self._character):
-            token = Token(TokenType.Minus, self._character, self._line)
+        elif match(r"^-$", self._character):
+            token = Token(TokenType.MINUS, self._character, self._line)
         # Token '*'
-        elif match(r'^\*$', self._character):
-            token = Token(TokenType.Multiplication, self._character, self._line)
+        elif match(r"^\*$", self._character):
+            token = Token(TokenType.MULTIPLICATION, self._character, self._line)
         # Token '/'
-        elif match(r'^\/$', self._character):
-            if self._peek_character() == '/':
-                token = self._make_two_character_token(TokenType.Comments)
-            token = Token(TokenType.Division, self._character, self._line)
+        elif match(r"^\/$", self._character):
+            if self._peek_character() == "/":
+                token = self._make_two_character_token(TokenType.COMMENTS)
+            token = Token(TokenType.DIVISION, self._character, self._line)
         # Token EOL
-        elif match(r'^$', self._character) and self._peek_character() != '':
+        elif match(r"^$", self._character) and self._peek_character() != "":
             token = Token(TokenType.EOL, self._character, self._line)
         # Token EOF
-        elif match(r'^$', self._character):
+        elif match(r"^$", self._character):
             token = Token(TokenType.EOF, self._character, self._line)
         # Token '('
-        elif match(r'^\($', self._character):
-            token = Token(TokenType.LParen, self._character, self._line)
+        elif match(r"^\($", self._character):
+            token = Token(TokenType.LPAREN, self._character, self._line)
         # Token ')'
-        elif match(r'^\)$', self._character):
-            token = Token(TokenType.RParen, self._character, self._line)
+        elif match(r"^\)$", self._character):
+            token = Token(TokenType.RPAREN, self._character, self._line)
         # Token '{'
-        elif match(r'^\{$', self._character):
-            token = Token(TokenType.LBrace, self._character, self._line)
+        elif match(r"^\{$", self._character):
+            token = Token(TokenType.LBRACE, self._character, self._line)
         # Token '}'
-        elif match(r'^\}$', self._character):
-            token = Token(TokenType.RBrace, self._character, self._line)
+        elif match(r"^\}$", self._character):
+            token = Token(TokenType.RBRACE, self._character, self._line)
         # Token '['
-        elif match(r'^\[$', self._character):
-            token = Token(TokenType.LBracket, self._character, self._line)
+        elif match(r"^\[$", self._character):
+            token = Token(TokenType.LBRACKET, self._character, self._line)
         # Token ']'
-        elif match(r'^\]$', self._character):
-            token = Token(TokenType.RBracket, self._character, self._line)
+        elif match(r"^\]$", self._character):
+            token = Token(TokenType.RBRACKET, self._character, self._line)
         # Token ','
-        elif match(r'^\,$', self._character):
-            token = Token(TokenType.Comma, self._character, self._line)
+        elif match(r"^\,$", self._character):
+            token = Token(TokenType.COMMA, self._character, self._line)
+        # Token '.'
+        elif match(r"^\.$", self._character):
+            token = Token(TokenType.DOT, self._character, self._line)
         # Token '<'
-        elif match(r'^<$', self._character):
-            if self._peek_character() == '=':
+        elif match(r"^<$", self._character):
+            if self._peek_character() == "=":
                 token = self._make_two_character_token(TokenType.LE)
             else:
                 token = Token(TokenType.LT, self._character, self._line)
         # Token '>'
-        elif match(r'^>$', self._character):
-            if self._peek_character() == '=':
+        elif match(r"^>$", self._character):
+            if self._peek_character() == "=":
                 token = self._make_two_character_token(TokenType.GE)
             else:
                 token = Token(TokenType.GT, self._character, self._line)
         # Token '!'
-        elif match(r'^!$', self._character):
-            if self._peek_character() == '=':
-                token = self._make_two_character_token(TokenType.NotEq)
+        elif match(r"^!$", self._character):
+            if self._peek_character() == "=":
+                token = self._make_two_character_token(TokenType.NOT_EQ)
             else:
-                token = Token(TokenType.Negation, self._character, self._line)
+                token = Token(TokenType.NEGATION, self._character, self._line)
         # Token for any letter
         elif self._is_letter(self._character):
             literal = self._read_identifier()
@@ -95,41 +97,41 @@ class Lexer:
         elif self._is_number(self._character):
             literal = self._read_number()
 
-            if self._character == '.':
+            if self._character == ".":
                 self._read_character()
                 sufix = self._read_number()
-                return Token(TokenType.Float, f'{literal}.{sufix}', self._line)
+                return Token(TokenType.FLOAT, f"{literal}.{sufix}", self._line)
 
-            return Token(TokenType.Integer, literal, self._line)
+            return Token(TokenType.INTEGER, literal, self._line)
         elif match(r"^\"|'$", self._character):
             literal = self._read_string()
 
-            return Token(TokenType.String, literal, self._line)
+            return Token(TokenType.STRING, literal, self._line)
         # Illegal Token
         else:
-            token = Token(TokenType.Illegal, self._character, self._line)
+            token = Token(TokenType.ILLEGAL, self._character, self._line)
         self._read_character()
 
         return token
 
     def _is_letter(self, character: str) -> bool:
         # Expresión regular para obtener todas las letras del alfabeto español
-        return bool(match(r'^[a-zA-Z_]$', character))
+        return bool(match(r"^[a-zA-Z_]$", character))
 
     def _is_number(self, character: str) -> bool:
         # Expresión regular para objeter todos los digitos
-        return bool(match(r'^\d$', character))
+        return bool(match(r"^\d$", character))
 
     def _make_two_character_token(self, token_type: TokenType) -> Token:
         prefix = self._character
         self._read_character()
         suffix = self._character
 
-        return Token(token_type, f'{prefix}{suffix}', self._line)
+        return Token(token_type, f"{prefix}{suffix}", self._line)
 
     def _read_character(self) -> None:
         if self._read_position >= len(self._source):
-            self._character = ''
+            self._character = ""
         else:
             self._character = self._source[self._read_position]
             # Los \n son contados como caracteres, son skipeados por
@@ -147,7 +149,7 @@ class Lexer:
         while self._is_letter(self._character) or self._is_number(self._character):
             self._read_character()
 
-        return self._source[initial_position:self._position]
+        return self._source[initial_position : self._position]
 
     def _read_number(self) -> str:
         initial_position = self._position
@@ -155,7 +157,7 @@ class Lexer:
         while self._is_number(self._character):
             self._read_character()
 
-        return self._source[initial_position:self._position]
+        return self._source[initial_position : self._position]
 
     def _read_string(self) -> str:
         # Vemos en cual tipo de comilla está el caracter actual
@@ -169,11 +171,12 @@ class Lexer:
 
         # Segumos leyendo el string hasta que encontremos otra comilla o se
         # acabe el archivo
-        while self._character != quote_type \
-                and self._read_position <= len(self._source):
+        while self._character != quote_type and self._read_position <= len(
+            self._source
+        ):
             self._read_character()
 
-        string = self._source[initial_position: self._position]
+        string = self._source[initial_position : self._position]
 
         self._read_character()
 
@@ -181,10 +184,14 @@ class Lexer:
 
     def _peek_character(self, skip=1) -> str:
         if self._read_position >= len(self._source):
-            return ''
+            return ""
 
-        return self._source[self._read_position] if skip == 1 else self._source[self._read_position + (skip - 1)]
+        return (
+            self._source[self._read_position]
+            if skip == 1
+            else self._source[self._read_position + (skip - 1)]
+        )
 
     def _skip_whitespace(self) -> None:
-        while match(r'^\s$', self._character):
+        while match(r"^\s$", self._character):
             self._read_character()
--- fol/token.py
+++ fol/token.py
@@ -1,8 +1,19 @@
-from enum import (
-    Enum,
-    unique,
-    auto
-)
+"""
+This module create two classes, TokenType and Token for create a Token,
+you have to pass it the token type, the literal and  the line where the
+token is, just for the case an error occurs.
+
+Classes:
+
+    TokenType(Enum)
+    Token(NamedTuple)
+
+Function:
+
+    lookup_token_type(literal: str) -> TokenType
+"""
+
+from enum import Enum, unique, auto
 from typing import (
     Dict,
     NamedTuple,
@@ -13,73 +24,74 @@ from typing import (
 class TokenType(Enum):
     """Define all the tokens types that the language have"""
 
-    String = auto()
-    Integer = auto()
-    Float = auto()
-    Boolean = auto()
-    false = auto()
-    true = auto()
-    Array = auto()
-    Object = auto()
-    Tuple = auto()
-    Null = auto()
-    Set = auto()
-    Identifier = auto()
-    If = auto()
-    Endif = auto()
-    Elseif = auto()
-    Else = auto()
-    And = auto()
-    Or = auto()
-    While = auto()
-    Endwhile = auto()
-    Do = auto()
-    For = auto()
-    Each = auto()
-    Endfor = auto()
-    MakeFunc = auto()
-    Return = auto()
-    Endfunc = auto()
-    Class = auto()
-    Endclass = auto()
-    Then = auto()
-    Interface = auto()
-    Endinterface = auto()
-    Minus = auto()
-    Negation = auto()
-    LParen = auto()
-    RParen = auto()
-    LBrace = auto()
-    RBrace = auto()
-    LBracket = auto()
-    RBracket = auto()
-    Plus = auto()
-    Multiplication = auto()
-    Division = auto()
-    Assign = auto()
-    LT = auto() # <
-    GT = auto() # >
-    LE = auto() # <=
-    GE = auto() # >=
-    Equals = auto()
-    NotEq = auto()
-    Dot = auto()
-    Comma = auto()
-    TPoints = auto() # :
-    LArrow = auto()
-    Illegal = auto()
-    Comments = auto() # //
+    STRING = auto()
+    INTEGER = auto()
+    FLOAT = auto()
+    BOOLEAN = auto()
+    FALSE = auto()
+    TRUE = auto()
+    ARRAY = auto()
+    OBJECT = auto()
+    TUPLE = auto()
+    NULL = auto()
+    SET = auto()
+    IDENTIFIER = auto()
+    IF = auto()
+    ENDIF = auto()
+    ELSEIF = auto()
+    ELSE = auto()
+    AND = auto()
+    OR = auto()
+    WHILE = auto()
+    ENDWHILE = auto()
+    DO = auto()
+    FOR = auto()
+    EACH = auto()
+    ENDFOR = auto()
+    FUNCTION = auto()
+    RETURN = auto()
+    ENDFUNC = auto()
+    CLASS = auto()
+    ENDCLASS = auto()
+    THEN = auto()
+    INTERFACE = auto()
+    ENDINTERFACE = auto()
+    MINUS = auto()
+    NEGATION = auto()
+    LPAREN = auto()
+    RPAREN = auto()
+    LBRACE = auto()
+    RBRACE = auto()
+    LBRACKET = auto()
+    RBRACKET = auto()
+    PLUS = auto()
+    MULTIPLICATION = auto()
+    DIVISION = auto()
+    ASSIGN = auto()
+    LT = auto()  # <
+    GT = auto()  # >
+    LE = auto()  # <=
+    GE = auto()  # >=
+    EQ = auto()  # ==
+    NOT_EQ = auto()  # !=
+    DOT = auto()
+    COMMA = auto()
+    TPOINTS = auto()  # :
+    LARROW = auto()
+    ILLEGAL = auto()
+    COMMENTS = auto()  # //
     EOL = auto()
     EOF = auto()
 
 
 class Token(NamedTuple):
     """Define a Token. Everytime a token is created, the token you have to pass it
-three arguments, which are:
+    three arguments, which are:
 
-    token_type: TokenType
-    literal: str
-    line: int (default=1)"""
+        token_type: TokenType
+        literal: str
+        line: int (default=1)"""
+
     token_type: TokenType
     literal: str
     line: int = 1
@@ -89,31 +101,40 @@ three arguments, which are:
 
 
 def lookup_token_type(literal: str) -> TokenType:
+    """
+    This function take the literal of the token and return the TokenType
+    that matches with the literal.
+
+    :param literal: The token itself
+    :type literal: str
+    :return: The TokenType that matches with the literal
+    :rtype: TokenType
+    """
     keywords: Dict[str, TokenType] = {
-        "set": TokenType.Set,
-        "false": TokenType.false,
-        "true": TokenType.false,
-        "null": TokenType.Null,
-        "if": TokenType.If,
-        "elseif": TokenType.Elseif,
-        "else": TokenType.Else,
-        "endif": TokenType.Endif,
-        "and": TokenType.And,
-        "or": TokenType.Or,
-        "while": TokenType.While,
-        "do": TokenType.Do,
-        "endwhile": TokenType.Endwhile,
-        "for": TokenType.For,
-        "each": TokenType.Each,
-        "endfor":  TokenType.Endfor,
-        "makeFunc": TokenType.MakeFunc,
-        "then": TokenType.Then,
-        "return": TokenType.Return,
-        "endfunc": TokenType.Endfunc,
-        "class": TokenType.Class,
-        "endclass": TokenType.Endclass,
-        "interface": TokenType.Interface,
-        "endinterface": TokenType.Endinterface,
+        "set": TokenType.SET,
+        "false": TokenType.FALSE,
+        "true": TokenType.TRUE,
+        "null": TokenType.NULL,
+        "if": TokenType.IF,
+        "elseif": TokenType.ELSEIF,
+        "else": TokenType.ELSE,
+        "endif": TokenType.ENDIF,
+        "and": TokenType.AND,
+        "or": TokenType.OR,
+        "while": TokenType.WHILE,
+        "do": TokenType.DO,
+        "endwhile": TokenType.ENDWHILE,
+        "for": TokenType.FOR,
+        "each": TokenType.EACH,
+        "endfor": TokenType.ENDFOR,
+        "makeFunc": TokenType.FUNCTION,
+        "then": TokenType.THEN,
+        "return": TokenType.RETURN,
+        "endfunc": TokenType.ENDFUNC,
+        "class": TokenType.CLASS,
+        "endclass": TokenType.ENDCLASS,
+        "interface": TokenType.INTERFACE,
+        "endinterface": TokenType.ENDINTERFACE,
     }
 
-    return keywords.get(literal, TokenType.Identifier)
+    return keywords.get(literal, TokenType.IDENTIFIER)
