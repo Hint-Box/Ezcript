b331d3d8ecb1d6fc9542eeb181183c3c166fba09
FRostri <FRostri@protonmail.com>
2021-04-06 19:32:22 -0500

    feat: Add keywords, strings and tests

--- ezcript/lexer/lexer.py
+++ ezcript/lexer/lexer.py
@@ -3,6 +3,7 @@ from re import match
 from ezcript.tokens import (
     Token,
     TokenType,
+    lookup_token_type,
 )
 
 
@@ -22,7 +23,8 @@ class Lexer:
         if match(r'^%$', self._character):
             if self._peek_character() == '=':
                 token = self._make_two_character_token(TokenType.PERCENTEQUAL)
-            token = Token(TokenType.PERCENT, self._character)
+            else:
+                token = Token(TokenType.PERCENT, self._character)
         elif match(r'^\($', self._character):
             token = Token(TokenType.LPAREN, self._character)
         elif match(r'^\)$', self._character):
@@ -67,7 +69,8 @@ class Lexer:
                     token = self._make_three_character_token(
                         TokenType.DOUBLESLASHEQUAL)
                 else:
-                    token = Token(TokenType.DOUBLESLASH, self._character)
+                    token = self._make_two_character_token(
+                        TokenType.DOUBLESLASH)
             elif self._peek_character() == '=':
                 token = self._make_two_character_token(TokenType.SLASHEQUAL)
             else:
@@ -102,6 +105,24 @@ class Lexer:
                 token = self._make_two_character_token(TokenType.NOTEQUAL)
             else:
                 token = Token(TokenType.NEGATION, self._character)
+        elif self._is_letter(self._character):
+            literal = self._read_identifier()
+            token_type = lookup_token_type(literal)
+
+            return Token(token_type, literal)
+        elif self._is_number(self._character):
+            literal = self._read_number()
+
+            if self._character == '.':
+                self._read_character()
+                sufix = self._read_number()
+                return Token(TokenType.NUMBER, f'{literal}.{sufix}')
+
+            return Token(TokenType.NUMBER, literal)
+        elif match(r'^\"|\'$', self._character):
+            literal = self._read_string()
+
+            return Token(TokenType.STRING, literal)
         else:
             token = Token(TokenType.ILLEGAL, self._character)
 
@@ -143,3 +164,55 @@ class Lexer:
     def _skip_whitespace(self) -> None:
         while match(r'^\s$', self._character):
             self._read_character()
+
+    def _read_string(self) -> str:
+
+        # With this, we can know which type of quote is the current character
+        quote_type = self._character
+
+        # We have to move one character more for get the string content
+        self._read_character()
+
+        # Save the current position for later save all the sting in a
+        # variable
+        initial_position = self._position
+
+        # We keep reading the string content until we find the closing
+        # quotation mark or the file end
+        while self._character != quote_type \
+                and self._read_position <= len(self._source):
+            self._read_character()
+
+        # After we read until the final of the string or the file, we
+        # save the content on the initial position until the current position
+        string = self._source[initial_position:self._position]
+
+        # Go to the last character which is the closing quotation mark
+        self._read_character()
+
+        return string
+
+    def _is_letter(self, character: str) -> bool:
+        # Expresión regular para obtener todas las letras del alfabeto español
+        return bool(match(r'^[a-zA-Z_]$', character))
+
+    def _is_number(self, character: str) -> bool:
+        # Expresión regular para objeter todos los digitos
+        return bool(match(r'^\d$', character))
+
+    def _read_identifier(self) -> str:
+        initial_position = self._position
+
+        while self._is_letter(self._character) or self._is_number(
+                self._character):
+            self._read_character()
+
+        return self._source[initial_position:self._position]
+
+    def _read_number(self) -> str:
+        initial_position = self._position
+
+        while self._is_number(self._character):
+            self._read_character()
+
+        return self._source[initial_position:self._position]
--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -57,3 +57,145 @@ class TestLexer:
         ]
 
         assert expected_tokens == tokens
+
+    def test_two_char_token(self) -> None:
+        source: str = """
+            2 ** 3
+            num1 *= 1
+            4++
+            num2 += 1
+            4--
+            num3 -= 1
+            23 // 2
+            num4 /= 1
+            3 <= 4
+            5 == 5
+            5 >= 1
+            true != false
+        """
+        lexer: Lexer = Lexer(source)
+
+        tokens: list[Token] = []
+        for i in range(34):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: list[Token] = [
+            Token(TokenType.NUMBER, "2"),
+            Token(TokenType.DOUBLESTAR, "**"),
+            Token(TokenType.NUMBER, "3"),
+            Token(TokenType.IDENTIFIER, "num1"),
+            Token(TokenType.STAREQUAL, "*="),
+            Token(TokenType.NUMBER, "1"),
+            Token(TokenType.NUMBER, "4"),
+            Token(TokenType.DOUBLEPLUS, "++"),
+            Token(TokenType.IDENTIFIER, "num2"),
+            Token(TokenType.PLUSEQUAL, "+="),
+            Token(TokenType.NUMBER, "1"),
+            Token(TokenType.NUMBER, "4"),
+            Token(TokenType.DOUBLEMINUS, "--"),
+            Token(TokenType.IDENTIFIER, "num3"),
+            Token(TokenType.MINEQUAL, "-="),
+            Token(TokenType.NUMBER, "1"),
+            Token(TokenType.NUMBER, "23"),
+            Token(TokenType.DOUBLESLASH, "//"),
+            Token(TokenType.NUMBER, "2"),
+            Token(TokenType.IDENTIFIER, "num4"),
+            Token(TokenType.SLASHEQUAL, "/="),
+            Token(TokenType.NUMBER, "1"),
+            Token(TokenType.NUMBER, "3"),
+            Token(TokenType.LESSEQUAL, "<="),
+            Token(TokenType.NUMBER, "4"),
+            Token(TokenType.NUMBER, "5"),
+            Token(TokenType.EQEQUAL, "=="),
+            Token(TokenType.NUMBER, "5"),
+            Token(TokenType.NUMBER, "5"),
+            Token(TokenType.GREATEREQUAL, ">="),
+            Token(TokenType.NUMBER, "1"),
+            Token(TokenType.BOOLEAN, "true"),
+            Token(TokenType.NOTEQUAL, "!="),
+            Token(TokenType.BOOLEAN, "false"),
+        ]
+
+        assert expected_tokens == tokens
+
+    def test_booleans(self) -> None:
+        source: str = """
+            true == true
+            false == false
+            true != false
+        """
+        lexer: Lexer = Lexer(source)
+
+        tokens: list[Token] = []
+        for i in range(9):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: list[Token] = [
+            Token(TokenType.BOOLEAN, "true"),
+            Token(TokenType.EQEQUAL, "=="),
+            Token(TokenType.BOOLEAN, "true"),
+            Token(TokenType.BOOLEAN, "false"),
+            Token(TokenType.EQEQUAL, "=="),
+            Token(TokenType.BOOLEAN, "false"),
+            Token(TokenType.BOOLEAN, "true"),
+            Token(TokenType.NOTEQUAL, "!="),
+            Token(TokenType.BOOLEAN, "false"),
+        ]
+
+        assert expected_tokens == tokens
+
+    def test_keywords(self) -> None:
+        source: str = """
+            set
+            if
+            then
+            elseif
+            else
+            endif
+            while
+            do
+            break
+            endwhile
+            for
+            each
+            endfor
+            makeFunc
+            return
+            endfunc
+            class
+            endclass
+            inherit
+            interface
+            endinterface
+        """
+        lexer: Lexer = Lexer(source)
+
+        tokens: list[Token] = []
+        for i in range(21):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: list[Token] = [
+            Token(TokenType.KEYWORD, "set"),
+            Token(TokenType.KEYWORD, "if"),
+            Token(TokenType.KEYWORD, "then"),
+            Token(TokenType.KEYWORD, "elseif"),
+            Token(TokenType.KEYWORD, "else"),
+            Token(TokenType.KEYWORD, "endif"),
+            Token(TokenType.KEYWORD, "while"),
+            Token(TokenType.KEYWORD, "do"),
+            Token(TokenType.KEYWORD, "break"),
+            Token(TokenType.KEYWORD, "endwhile"),
+            Token(TokenType.KEYWORD, "for"),
+            Token(TokenType.KEYWORD, "each"),
+            Token(TokenType.KEYWORD, "endfor"),
+            Token(TokenType.KEYWORD, "makeFunc"),
+            Token(TokenType.KEYWORD, "return"),
+            Token(TokenType.KEYWORD, "endfunc"),
+            Token(TokenType.KEYWORD, "class"),
+            Token(TokenType.KEYWORD, "endclass"),
+            Token(TokenType.KEYWORD, "inherit"),
+            Token(TokenType.KEYWORD, "interface"),
+            Token(TokenType.KEYWORD, "endinterface"),
+        ]
+
+        assert expected_tokens == tokens
