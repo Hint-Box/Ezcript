e77d6e47865b047a26edf38791ce71c3dcc6773a
FRostri <FRostri@protonmail.com>
2021-06-06 14:26:47 -0500

    feat: the interpreter now have a prompt The interpreter do not read identifiers yet.

--- interpreter/ezcript/src/main.rs
+++ interpreter/ezcript/src/main.rs
@@ -3,13 +3,12 @@ use clap::{App, Arg};
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{stdin, stdout, BufReader};
-use std::process::exit;
 
 use ezcript_lexer::{
     lexer::Lexer,
     tokens::{Token, TokenKind},
 };
-use ezcript_result::{Error, Result};
+use ezcript_result::Result;
 
 fn main() -> Result<()> {
     let args = App::new("Ezcript-lang")
@@ -17,8 +16,6 @@ fn main() -> Result<()> {
         .author("Author: DaBitwisersWay")
         .about("\"A friendly programming language whose purpose is to teach new users in the tech world about the basics of programming languages and their use.\"")
         .arg(Arg::with_name("file")
-            .short("f")
-            .long("file")
             .value_name("FILE")
             .help("Sets the script for execute by the interpreter")
             .required(false)
@@ -28,12 +25,44 @@ fn main() -> Result<()> {
     let file_name = args.value_of("file");
 
     if file_name == None {
-        // run_prompt()?;
-        println!("Hola, no hay file");
+        run_prompt()?;
     } else {
         run_file(file_name)?;
     }
 
+    Ok(())
+}
+
+fn run_prompt() -> Result<()> {
+    println!(
+        "Welcom to Ezcript v1.0.0
+Type \"help\" for more information"
+    );
+    loop {
+        let mut source = String::new();
+        print!(">> ");
+        stdout().flush()?;
+        stdin().read_line(&mut source).expect("Failed to read line");
+        source.remove(source.len() - 1);
+        let mut lexer = Lexer::new(source.chars());
+        if source == "exit".to_string() {
+            break;
+        } else if source == "help" {
+            println!("Some help message");
+        } else {
+            let mut tokens: Vec<Token> = Vec::new();
+            for _i in 0..source.len() {
+                let token: Token = lexer.next_token().unwrap().unwrap();
+                if token.kind == TokenKind::Eof {
+                    break;
+                }
+                tokens.push(token);
+            }
+            for token in tokens {
+                println!("{}", token);
+            }
+        }
+    }
     Ok(())
 }
 
@@ -43,7 +72,7 @@ fn run_file(file_name: Option<&str>) -> Result<()> {
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents)?;
     let mut lexer = Lexer::new(contents.chars());
-    for i in 0..contents.len() {
+    for _i in 0..contents.len() {
         let token = lexer.next_token().unwrap().unwrap();
         if token.kind != TokenKind::Eof {
             println!("{}", token);
